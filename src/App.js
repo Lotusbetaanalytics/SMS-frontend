47be7830d9f2a3c5111670cdbcacf79d4c9eed05
Ousjiali <fonsusali@yahoo.com>
2022-03-07 23:17:16 +0100

    Sidebar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Input from "./components/Input";
+import Dashboard from "./screens/admin/Dashboard";
 import LoginPage from "./screens/admin/LoginPage";
 
 function App() {
   return (
     <Router>
-
       <Routes>
         <Route path="/" exact element={<LoginPage />} />
         <Route path="/input" exact element={<Input />} />
+        <Route path="/admin/dashboard" exact element={<Dashboard />} />
       </Routes>
     </Router>
   );
